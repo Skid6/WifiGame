cb2567add615b6681bc6728d2e10e366aa745a83
Skid6 <89701185+Skid6@users.noreply.github.com>
2021-08-28 12:31:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -71,7 +71,7 @@ function setup() {
 
   
   trex.setCollider("rectangle",0,0,trex.width,trex.height);
-  trex.debug = true
+  trex.debug = false
   
   score = 0;
   
@@ -102,7 +102,7 @@ function draw() {
     }
     
     //jump when the space key is pressed
-    if(keyDown("space")&& trex.y >= 100) {
+    if(keyDown("space")&& trex.y >= 161) {
         trex.velocityY = -12;
         jumpSound.play();
     }
